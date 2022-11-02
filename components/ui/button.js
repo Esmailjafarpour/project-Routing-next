c15d03bdd4e1eba13fb1b879b8638a9ab4d569ea
nader <esmailjafarpour29@gmail.com>
2022-11-02 14:04:16 +0330

    search component for filtering

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,13 +2,23 @@ import Link from 'next/link';
 import styleButton from './button.module.css';
 
 function Button(props) {
+    if (props.link) {
+        return(
+            <Link legacyBehavior href={props.link}>
+                <a className={styleButton.btn}>
+                    {props.children} 
+                </a>
+            </Link>
+        )
+    }
+
     return(
-        <Link legacyBehavior href={props.link}>
-            <a className={styleButton.btn}>
-                 {props.children} 
-            </a>
-        </Link>
+        <button className={styleButton.btn} onClick={props.onClick}>
+                {props.children} 
+        </button>
     )
+    
+    
 }
 
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list.jsx';
+import EventSearch from '../../components/events/event-search.jsx';
+
 
 function AllEventsPage() {
 
    const events = getAllEvents();
     return (
-      <div>
+      <>
+         <EventSearch/>
          <EventList items={events}/>
-      </div>
+      </>
     )
   }
   

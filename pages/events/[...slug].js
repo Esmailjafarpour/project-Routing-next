13c9817b52521dba5738e692279dc208de3ad2af
nader <esmailjafarpour29@gmail.com>
2022-11-08 19:53:05 +0330

    clientServerSiteRendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,5 @@
-import { Fragment } from 'react';
+import { Fragment,useState,useEffect } from 'react';
 import { useRouter } from 'next/router';
-
 import { getFilteredEvents } from '../../dummy-data';
 // import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
@@ -8,8 +7,95 @@ import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
+// import useSWR from 'swr';
+
 function FilteredEventsPage(props) {
-  const router = useRouter();
+
+//  //clientServerSiteRendering
+//  const [loadedEvents ,setLoadedEvents] = useState()
+//  const router = useRouter();
+//  const filterData = router.query.slug
+//  // const {data,error} = useSWR("")
+//  const filteredEvents = getFilteredEvents();
+//  useEffect(()=>{
+//    if (filteredEvents) {
+//      const events = [];
+//      for (const key in filteredEvents) {
+//        events.push({
+//          id : key,
+//          ...filteredEvents[key]
+//        })
+//      }
+//    }
+//    setLoadedEvents(events)
+//  },[filteredEvents])
+
+//   if (!loadedEvents) {
+//     return <p className='center'>Loading...</p>;
+//   }
+
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
+
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
+
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12||
+//     error
+//   ) {
+//     return (
+//       <Fragment>
+//         <ErrorAlert>
+//           <p>Invalid filter. Please adjust your values!</p>
+//         </ErrorAlert>
+//         <div className='center'>
+//           <Button link='/events'>Show All Events</Button>
+//         </div>
+//       </Fragment>
+//     );
+//   }
+  
+//   const filteredEvents = loadedEvents.filter((event) => {
+//     const eventDate = new Date(event.date);
+//     return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+//   });
+
+//   if (!filteredEvents || filteredEvents.length === 0) {
+//     return (
+//       <Fragment>
+//         <ErrorAlert>
+//           <p>No events found for the chosen filter!</p>
+//         </ErrorAlert>
+//         <div className='center'>
+//           <Button link='/events'>Show All Events</Button>
+//         </div>
+//       </Fragment>
+//     );
+//   }
+
+//   const date = new Date(numYear, numMonth - 1)
+
+//   return (
+//     <Fragment>
+//       <ResultsTitle date={date} />
+//       <EventList items={filteredEvents} />
+//     </Fragment>
+//   );
+
+
+
+
+
+
+
+  //method-one
+  // const router = useRouter();
 
   //method-one
 
@@ -48,20 +134,27 @@ function FilteredEventsPage(props) {
   // }
 
 
-  //method-one
+  // // method-one
 
   // const filteredEvents = getFilteredEvents({
   //   year: numYear,
   //   month: numMonth,
   // });
 
-  //method-one
+  // // method-one
 
   // const date = new Date(numYear, numMonth - 1);
 
 
 
-  //method-two
+
+
+
+
+
+
+
+  // // method-two
   if (props.hasError) {
     return (
             <Fragment>
@@ -75,7 +168,7 @@ function FilteredEventsPage(props) {
         )
   }
 
-  //method-two
+ // // method-two
   const filteredEvents = props.events
 
   if (!filteredEvents || filteredEvents.length === 0) {
@@ -92,19 +185,18 @@ function FilteredEventsPage(props) {
   }
 
  
-  //method-two
+  // // method-two
   const date = new Date(props.date.numYear, props.date.numMonth - 1)
-
   return (
     <Fragment>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
   );
+
 }
 
-
-//method-two
+// // method-two
 export async function getServerSideProps(context){
 
   const {params} = context;
@@ -155,5 +247,6 @@ export async function getServerSideProps(context){
     }
   }
 }
+
 
 export default FilteredEventsPage;
